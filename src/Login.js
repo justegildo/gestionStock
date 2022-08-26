563edae5cf967a3a1ddf7683f4779441997e42b0
Dossou Gildas <dossousedjrogildas@gmail.com>
2022-08-26 17:35:40 +0100

    Update

--- src/Login.js
+++ src/Login.js
@@ -6,6 +6,7 @@ import { Link, useHistory } from 'react-router-dom';
 import AuthenticationService from './services/AuthenticationService';
 import axios from 'axios';
 
+
 import apiUrl from './baseUrls/functions';
 import AxiosService from './axiosConfig/AxiosService';
 
@@ -35,36 +36,35 @@ function Login(props) {
 
         setTimeout(() => {
             setLoading1(false);
-        }, 1000);
+        }, 2000);
     }
     
     return (
-        <div >
-            <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
-                <div className="text-center mb-5">
-                    <img src="images/blocks/logos/hyper.svg" alt="hyper" height="50" className="mb-3" />
-                    <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                    <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                    <a className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</a>
-                </div>
-
-                <div>
-                    <label htmlFor="username" className="block text-900 font-medium mb-2">Nom d'utilisateur</label>
-                    <InputText id="username" type="text" className="w-full mb-3" onChange={bind} required />
-
-                    <label htmlFor="password" className="block text-900 font-medium mb-2">Mot de passe</label>
-                    <InputText id="password" type="password" className="w-full mb-3" onChange={bind} required />
-
-                    <div className="flex align-items-center justify-content-between mb-6">
-                        <div className="flex align-items-center">
-                            {/* <Checkbox inputId="rememberMe" binary className="mr-2" onChange={bind} checked={checked} /> */}
-                            <Checkbox inputId="rememberMe" binary className="mr-2" onChange={e => setChecked(e.checked)} checked={checked} />
-                            <label htmlFor="rememberme1">Se souvenir de moi</label>
-                        </div>
-                        <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer" style={{display: 'none', visibility: 'hidden'}} >Forgot password?</a>
+        <div className=''>
+            <div className="flex justify-content-center mt-6">
+                <div className="surface-card p-5 shadow-2 w-full lg:w-3 border-round w-6/12 h-4 ">
+                    <div className="text-center mb-5">
+                        <img src="images/blocks/logos/hyper.svg" alt="hyper" height="50" className="mb-3" />
+                        <div className="text-900 text-3xl font-medium mb-3">Connexion</div>
                     </div>
-                    <Button label="Se connecter" icon="pi pi-user" className="w-full" loading={loading1} onClick={()=> {authenticate() ; onLoadingClick1()}}/>
-                    {/* <Link to="/dashboard"><Button label="Sign In" icon="pi pi-user" className="w-full" onClick={authenticate}/></Link> */}
+
+                    <div>
+                        <label htmlFor="username" className="block text-900 font-medium mb-2">Nom d'utilisateur</label>
+                        <InputText id="username" type="text" className="w-full mb-3" onChange={bind} required />
+
+                        <label htmlFor="password" className="block text-900 font-medium mb-2">Mot de passe</label>
+                        <InputText id="password" type="password" className="w-full mb-3" onChange={bind} required />
+
+                        <div className="flex align-items-center justify-content-between mb-6">
+                            <div className="flex align-items-center">
+                                {/* <Checkbox inputId="rememberMe" binary className="mr-2" onChange={bind} checked={checked} /> */}
+                                {/*<Checkbox inputId="rememberMe" binary className="mr-2" onChange={e => setChecked(e.checked)} checked={checked} />
+                                <label htmlFor="rememberme1">Se souvenir de moi</label>*/}
+                            </div>
+                        </div>
+                        <Button label="Se connecter" icon="pi pi-user" className="w-full" loading={loading1} onClick={()=> {authenticate() ; onLoadingClick1()}}/>
+                        {/* <Link to="/dashboard"><Button label="Sign In" icon="pi pi-user" className="w-full" onClick={authenticate}/></Link> */}
+                    </div>
                 </div>
             </div>
         </div>
